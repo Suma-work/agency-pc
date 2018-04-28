0e58d4b78308ddaf45d2954f2d2e438e5da140ab
JasonZhlu <3027089725@qq.com>
2018-04-28 09:58:57 +0800

    金融服务

--- src/main/resources/statics/js/modules/sys/bank.js
+++ src/main/resources/statics/js/modules/sys/bank.js
@@ -50,14 +50,15 @@ function beforeSelectRow() {
     return(true);  
 } 
 function clearNoNum(obj){ 
-    obj.value = obj.value.replace(/[^\d.]/g,"");  //清除“数字”和“.”以外的字符  
-    obj.value = obj.value.replace(/\.{2,}/g,"."); //只保留第一个. 清除多余的  
-    obj.value = obj.value.replace(".","$#$").replace(/\./g,"").replace("$#$","."); 
-    obj.value = obj.value.replace(/^([1-9]\d{0,1}(\.\d{1,2})|100)$/,"");//只能输入两个小数  
-    if(obj.value.indexOf(".")< 0 && obj.value !=""){//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额 
-        obj.value= parseFloat(obj.value); 
-    } 
-//	var reg=/^([1-9]\d{0,1}(\.\d{1,2})|100)$/;
+	console.log(obj.value);
+	 obj.value = obj.value.replace(/[^\d.]/g,"");  //清除“数字”和“.”以外的字符  
+	 obj.value = obj.value.replace(/\.{2,}/g,"."); //只保留第一个. 清除多余的  
+	 obj.value = obj.value.replace(".","$#$").replace(/\./g,"").replace("$#$","."); 
+	 obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/,'$1$2.$3');//只能输入两个小数  
+	 if(obj.value.indexOf(".")< 0 && obj.value !=""){//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额 
+	    obj.value= parseFloat(obj.value); 
+	 }  
+//	var reg=/^([1-9]{1}[0-9]{0,1}|0|100)(.\d{1,2}){0,1}$/;
 //	if(reg.test(obj.value)){
 //	   	console.log("success");
 //	}else{
